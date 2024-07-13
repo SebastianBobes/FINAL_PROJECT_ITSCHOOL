e5aea258c840412fc8c07fd4483a2f3183821efc
SebastianBobes <bobes_sebastian03@yahoo.com>
2024-07-13 16:59:08 +0300

    Initial commit

---
+++ Members.py
@@ -0,0 +1,47 @@
+class Member:
+    def __init__(self, last_name: str ,first_name: str,
+                 telephone_number: str,study_year: str,
+                 college: str, middle_name: str, events: list = [] , department_id: int = None):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.telephone_number = telephone_number
+        self.study_year = study_year
+        self.college = college
+        self.department_id = department_id
+        self.events = events
+        self.email = self.create_email_address()
+    def create_email_address(self):
+        if self.middle_name == '':
+            email = f"{self.first_name.lower()}.{self.last_name.lower()}@euroavia-bucuresti.ro"
+        else:
+            email = f"{self.first_name.lower()}-{self.middle_name.lower()}.{self.last_name.lower()}@euroavia-bucuresti.ro"
+        return email
+
+class Heads(Member):
+    def __init__(self, last_name: str, first_name: str, telephone_number: str, study_year: str, college: str,
+                 middle_name:str,department_head_of: str = None):
+        super().__init__(last_name, first_name, middle_name, telephone_number, study_year, college)
+        self.department_head_of = department_head_of
+
+
+class Member_of_local_board(Heads):
+    def __init__(self, last_name: str, first_name: str, middle_name:str, telephone_number: str, study_year: str, college: str,
+                 position: str):
+        super().__init__(last_name, first_name,middle_name, telephone_number, study_year, college)
+        self.position = position
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ euroavia_db.py
@@ -0,0 +1,47 @@
+import json
+import psycopg2 as ps
+from Members import Member
+
+
+
+def execute_query(sql_query: str, config: dict):
+    try:
+        with ps.connect(**config) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(sql_query)
+                return cursor.statusmessage
+    except Exception as e:
+        print(f"Failure on reading from database. Error: {e}")
+        return False
+
+def insert_into_db(member:Member,path: str = "config.json"):
+    with open(path, "r") as f:
+        config = json.loads(f.read())['config_db']
+    with ps.connect(**config) as conn:
+        with conn.cursor() as cursor:
+            sql_query = ("INSERT into euroavia.members (last_name, first_name, middle_name,telephone_number,study_year,college,department_id,email_address)"
+                         f"values('{member.last_name}','{member.first_name}','{member.middle_name}','{member.telephone_number}','{member.study_year}', '{member.college}',{member.department_id}, '{member.email}')")
+            cursor.execute(sql_query)
+
+
+
+# def read_from_database(sql_query: str,config: dict):
+#     try:
+#         with ps.connect(**config) as conn:
+#             with conn.cursor() as cursor:
+#                 cursor.execute(sql_query)
+#                 response = cursor.fetchall()
+#                 columns = [item.name for item in cursor.description]
+#                 new_data = []
+#                 for employee in response:
+#                     new_data.append(dict(zip(columns, employee)))
+#                 return new_data
+#     except Exception as e:
+#         print(f"Failure on reading from database. Error: {e}")
+
+
+if __name__ == '__main__':
+    pass
+
+
+
